d011397c6c7e4999433bdfd5cbe1b1a49a0ea193
extasy44 <seoheejun@gmail.com>
2020-10-24 23:46:32 +1100

    caching and versioning" ;

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,15 +1,24 @@
+
 var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js', { scope: '/help/' })
+    .register('/sw.js')
     .then(function () {
-      console.log('service worker registered');
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-window.addEventListener('beforeinstallprompt', function (event) {
+window.addEventListener('beforeinstallprompt', function(event) {
   console.log('beforeinstallprompt fired');
   event.preventDefault();
+  deferredPrompt = event;
   return false;
 });
